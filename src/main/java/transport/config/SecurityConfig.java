23a75ebc10d00e3ea127ae466b66094981b14de5
venkateshkona3456-oss <venkateshkona3456@gmail.com>
2026-08-22 17:58:07 +0530

    Fix JwtAuthenticationFilter import in SecurityConfig

--- src/main/java/transport/config/SecurityConfig.java
+++ src/main/java/transport/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package transport.config;
-
+import transport.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
