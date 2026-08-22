cb9f8ee734370d891afb51155fbbe3591ffe8ae9
venkateshkona3456-oss <venkateshkona3456@gmail.com>
2026-08-22 16:53:28 +0530

    fix cors for all vercel urls

--- src/main/java/transport/config/SecurityConfig.java
+++ src/main/java/transport/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-       configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://192.168.137.1:8080", "http://192.168.137.1:8081", "https://frontend-ten-eta-wsfo93g3bh.vercel.app"));
+       configuration.setAllowedOriginPatterns(List.of("https://*.vercel.app", "http://localhost:5173", "http://192.168.137.1:8080", "http://192.168.137.1:8081"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
